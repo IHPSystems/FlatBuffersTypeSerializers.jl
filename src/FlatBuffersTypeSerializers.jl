93f8be8346d10ff4947ad744e1e008ab07641c85
Jesper Stemann Andersen <jsa@ihp.dk>
2021-04-27 11:32:58 +0200

    Fixup FlatBuffersTypeSerializers

--- src/FlatBuffersTypeSerializers.jl
+++ src/FlatBuffersTypeSerializers.jl
@@ -7,8 +7,8 @@ abstract type FlatBuffersTypeSerializer{T} <: AbstractTypeSerializer{T} end
 
 function serialize(::Type{TSerializer}, value::T)::IO where {T, TSerializer <: FlatBuffersTypeSerializer{T}}
     buffer = FlatBuffers.build!(value)
-    data = FlatBuffers.finishedbytes(buffer)
-    return IOBuffer(data, read=true, write=true)
+    data = FlatBuffers.bytes(buffer)
+    return IOBuffer(data, read = true, write = true)
 end
 
 function deserialize(::Type{TSerializer}, stream::IO)::T where {T, TSerializer <: FlatBuffersTypeSerializer{T}}
